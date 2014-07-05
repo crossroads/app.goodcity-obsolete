9ba4545446cd960824d40ed3781d9c8eec917f8c
Steve Kenworthy <steveyken@gmail.com>
2014-07-05 17:35:58 +0800

    Item is now saving to API. Not associating with offer just yet.

--- app/controllers/items/new.js
+++ app/controllers/items/new.js
@@ -3,23 +3,32 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   actions: {
     createItem: function() {
-      // Get the Item title set by the "New Item" text field
-      var title = this.get('newTitle');
-      if (!title.trim()) { return; }
+
+      var donorDescription = this.get('donorDescription');
+      if (!donorDescription.trim()) { return; }
 
       // Create the new Item model
-      var item = this.store.createRecord('item', {
-        title: title,
+      var store = this.store;
+      var item = store.createRecord('item', {
+        donorDescription: donorDescription,
         isCompleted: false
       });
 
+      // set item relationship on offer
+      // NOTE: NOT WORKING QUITE YET - STEVE
+      var offerId = this.get('offerId');
+      store.find('offer', offerId).then(function(offer) {
+        item.set('offer', offer);
+      });
+
       // Clear the "New Item" text field
-      this.set('newTitle', '');
+      this.set('donorDescription', '');
 
       // Save the new model
       item.save();
 
-      this.transitionToRoute('offer');
+      // Go back to the main offer
+      this.transitionToRoute('offer', offerId);
     }
   }
 });
