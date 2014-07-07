4a9e37c0ecf5a4de9eb9e8e34268519911bcaa20
swatijadhav <swati.jadhav@kiprosh.com>
2014-07-07 16:08:51 +0530

    radio-button view for item conditions

--- app/controllers/items/new.js
+++ app/controllers/items/new.js
@@ -1,23 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  donorCondition: "New",
   needs: ["offer"],
   actions: {
     createItem: function() {
 
       var donorDescription = this.get('donorDescription');
       if (!donorDescription.trim()) { return; }
+      var donorCondition = this.get('donorCondition');
 
       // Create the new Item model
       var offer_id = this.get('controllers.offer').get('id');
       var offer = this.store.getById('offer', offer_id);
       var item = this.store.createRecord('item', {
         donorDescription: donorDescription,
+        donorCondition: donorCondition,
         offer: offer
       });
 
       // Clear the "New Item" text field
       this.set('donorDescription', '');
+      this.set('donorCondition', 'New');
 
       // Save the new model
       var route = this;
