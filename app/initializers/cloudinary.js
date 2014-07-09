0297669f2676249f320c610566c381021161e668
Shivani Thakur <shivanibhanwal@gmail.com>
2014-07-09 14:47:18 +0530

    Added cloudinary npm package to the app. Created initializer for the cloudinary though it is partially done at the moment :anguished:. Added to Broc file to load Cloudinary assets

---
+++ app/initializers/cloudinary.js
@@ -0,0 +1,7 @@
+export default {
+  name: 'cloudinary',
+  initialize: function(/* container, app */ ) {
+    Ember.$.cloudinary.config({'cloud_name': 'kiprosh',
+                              'api_key': 457459653293635});
+  }
+};
