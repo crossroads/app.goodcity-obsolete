05ed62cfabfe90f0c73475326b796d7349a0a8ab
swatijadhav <swati.jadhav@kiprosh.com>
2014-07-18 19:19:18 +0530

    complete pin authentication process

--- app/controllers/authenticate.js
+++ app/controllers/authenticate.js
@@ -2,25 +2,35 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    authenticate: function(){
+
+    authenticateUser: function(){
+      Ember.$('.auth_error').hide();
       var user_pin = this.get('pin');
+      var route = this;
 
       Ember.$.ajax({
         type: 'POST',
         url: GoodcityENV.APP.SERVER_PATH +"/verify",
-        data: {pin: user_pin},
+        data: {pin: user_pin, token: localStorage.step1_token},
         dataType: 'json',
         headers: {
           'Authorization': 'Bearer ' + localStorage.step1_token
         },
         success: function(data){
-          delete localStorage.step1_token;
-          localStorage.jwt = data.jwt_token;
+          if(data.error && data.error.length > 0) {
+            Ember.$('.auth_error').show();
+          } else {
+            delete localStorage.step1_token;
+            localStorage.jwt = data.jwt_token;
+            route.transitionToRoute('offers');
+          }
         },
         failure: function(){
+          Ember.$('.auth_error').show();
           alert("Unable to authenticate");
         }
       });
     }
   }
+
 });
--- app/controllers/register.js
+++ app/controllers/register.js
@@ -28,53 +28,5 @@ export default Ember.Controller.extend({
       });
 
     }
-  },
-
-  init: function(){
-
-    Ember.$().ready(function (){
-      set_button_visiblity();
-      validate_phone();
-      Ember.$('#mobile, #first_name, #last_name').keyup(set_button_visiblity);
-    });
-
-    function set_button_visiblity(){
-      var filled = (Ember.$('#mobile').val().length > 0 &&
-        Ember.$('#first_name').val().length > 0 &&
-        Ember.$('#last_name').val().length > 0);
-      Ember.$("button").prop("disabled", !filled);
-    }
-
-    function validate_phone(){
-      Ember.$('#mobile').focusout(function(){
-        var phone = Ember.$(this).val();
-        if(Ember.$.trim(phone).length > 0) {
-          if(phone.match(/^\+\d+$/)) {
-            check_uniqness(phone);
-          } else {
-            highlight_phone_field();
-          }
-        }
-      });
-    }
-
-    function check_uniqness(phone){
-      Ember.$.get(GoodcityENV.APP.SERVER_PATH +"/users/check_mobile", {mobile: phone}).done(function(data){
-          var is_uniq = data.is_unique_mobile;
-          is_uniq ? remove_highlight() : highlight_phone_field(true);
-      });
-    }
-
-    function highlight_phone_field(is_duplicate){
-      Ember.$('#mobile').addClass('invalid_input');
-      var error = is_duplicate ? 'Sorry, this number is already registered!' : 'Please include a valid phone number!';
-      Ember.$('#mobile_error').text(error);
-    }
-
-    function remove_highlight(){
-      Ember.$('#mobile').removeClass('invalid_input');
-      Ember.$('#mobile_error').empty();
-    }
-
   }
 });
