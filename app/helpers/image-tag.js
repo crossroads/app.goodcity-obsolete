1313782a2a0a986ca67960510e2fdf381a71e491
Shivani Thakur <shivanibhanwal@gmail.com>
2014-07-30 15:43:31 +0530

    Fixed cloudinary thumb cropping style for the image upload page

--- app/helpers/image-tag.js
+++ app/helpers/image-tag.js
@@ -1,13 +1,13 @@
 import Ember from "ember";
 
 export default Ember.Handlebars.makeBoundHelper(function(value, options) {
-
   var tag = Ember.$.cloudinary.image(value.split('/')[1], {
     version: value.split('/')[0],
     height: options.hash['height'],
     width: options.hash['width'],
     class: 'current_image '+ options.hash['className'],
-    crop: (options.hash['width'] === '50' ? 'fill' : 'fit'),
+    crop: (options.hash['width'] === 50 ? 'fill' : 'fit'),
+    border: (options.hash['width'] === 50 ? '2px_solid_grey' : '0px_solid_grey'),
     id: value
   });
 
