6606979244f35375ae831ba01b6be92f9c7fcaca
Shivani Thakur <shivanibhanwal@gmail.com>
2014-07-31 18:19:15 +0530

    Missing semicolon

--- app/helpers/favourite-star.js
+++ app/helpers/favourite-star.js
@@ -5,11 +5,11 @@ export default Ember.Handlebars.makeBoundHelper(function(current_image, favourit
   var image_tag;
   if(options.hash['isNew']) {
     if(favourite === current_image){
-      image_tag = "<img src='assets/images/star_circle.png' class='fav_star'></img>"
+      image_tag = "<img src='assets/images/star_circle.png' class='fav_star'></img>";
     }
   }
 
-  var image = image_tag ? (new Ember.Handlebars.SafeString(image_tag)) : null
+  var image = image_tag ? (new Ember.Handlebars.SafeString(image_tag)) : null;
   return image;
 
 });
