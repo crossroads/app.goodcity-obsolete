a30753dbff77f4e2f21e701b556fb3184fea1965
Steve Kenworthy <steveyken@gmail.com>
2014-08-02 12:11:01 +0800

    Updated logic in defaultImageURL to return any image if no favourite is selected.

--- app/models/item.js
+++ app/models/item.js
@@ -22,13 +22,25 @@ export default DS.Model.extend({
   //input to store image public-ids
   imageIdentifiers:     attr('string'),
   favouriteImage:       attr('string'),
+  //~ favouriteImage:       function() {
+    //~ var images = this.get('images');
+    //~ return (images && images.findBy('favourite', 'true'));
+  //~ }.property('this.images.@each'),
 
-  defaultImage: function() {
+  // favouriteImage or first image or placeholder image
+  defaultImageURL: function() {
+    var image_url
     var images    = this.get('images');
-    var fav_image = images && images.findBy('favourite', 'true');
-    var image_url = fav_image ? fav_image.get('thumbImageUrl') : "assets/images/default_item.jpg";
-
+    //~ var favourite_image = this.get('favouriteImage');
+    var favourite_image = images && images.findBy('favourite', 'true');
+    if (favourite_image) {
+      image_url = favourite_image.get('thumbImageUrl');
+    } else if (images.get('length') > 0) {
+      image_url = this.get('images.firstObject').get('thumbImageUrl');
+    } else {
+      image_url = "assets/images/default_item.jpg";
+    }
     return image_url;
-  }.property('this.images.@each')
+  }.property('this.images.@each'),
 
 });
