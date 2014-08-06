bad9d6db3f047070ab02fced142852631573e110
Steve Kenworthy <steveyken@gmail.com>
2014-08-06 13:01:14 +0800

    Removed isNew from favourite-star. Didn't seem like it was being used.

--- app/helpers/favourite-star.js
+++ app/helpers/favourite-star.js
@@ -1,12 +1,10 @@
 import Ember from "ember";
 
-export default Ember.Handlebars.makeBoundHelper(function(current_image_id, favourite_image_id, options) {
+export default Ember.Handlebars.makeBoundHelper(function(current_image_id, favourite_image_id) {
 
   var image_tag;
-  if(options.hash['isNew']) {
-    if(favourite_image_id === current_image_id){
-      image_tag = "<img src='assets/images/star_circle.png' class='fav_star'></img>";
-    }
+  if(favourite_image_id === current_image_id){
+    image_tag = "<img src='assets/images/star_circle.png' class='fav_star'></img>";
   }
 
   return image_tag ? (new Ember.Handlebars.SafeString(image_tag)) : null;
