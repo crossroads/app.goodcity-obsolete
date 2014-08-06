7f3ed2c62fec165cc1130d934b3d20547a13aa93
Steve Kenworthy <steveyken@gmail.com>
2014-08-06 13:14:48 +0800

    Converted donor_conditions radio buttons into a component to isolate state.

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -2,8 +2,4 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
 
-  donorConditions: function() {
-    return this.store.all('donor_condition').sortBy('id');
-  }.property('donor_condition.[]'),
-
 });
