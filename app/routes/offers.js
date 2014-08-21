31037a305857a0f9f7217eae2634ee05d93d8a72
swatijadhav <swati.jadhav@kiprosh.com>
2014-08-21 20:57:17 +0530

    create and redirect to new offer for new user

--- app/routes/offers.js
+++ app/routes/offers.js
@@ -1,6 +1,21 @@
 import AuthorizeRoute from './authorize';
 
 export default AuthorizeRoute.extend({
+
+  beforeModel: function() {
+    var route = this;
+    this.store.find('offer').then(function(my_offers){
+      var count = my_offers.get('length');
+      if ( count === 0) {
+        route.transitionTo('offers.new');
+      } else {
+        if (count === 1 && my_offers.get('firstObject.state') === 'draft') {
+          route.transitionTo('offer', my_offers.get('firstObject.id'));
+        }
+      }
+    });
+  },
+
   model: function() {
     return this.store.find('offer');
   }
