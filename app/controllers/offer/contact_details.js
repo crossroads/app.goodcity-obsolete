7491318599b65300ad4e00e8326cfc9fe2197608
swatijadhav <swati.jadhav@kiprosh.com>
2014-08-28 17:37:00 +0530

    territory and district selection list for contact-details page

--- app/controllers/offer/contact_details.js
+++ app/controllers/offer/contact_details.js
@@ -3,6 +3,13 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   needs: ["offer"],
 
+  selectedTerritory: {id: null},
+  selectedDistrict: {id: null},
+
+  territories: function(){
+    return this.store.findAll('territory');
+  }.property(),
+
   name: function(){
     return this.get('controllers.offer').get('userName');
   }.property(),
@@ -11,9 +18,18 @@ export default Ember.ObjectController.extend({
     return this.get('controllers.offer').get('userPhone');
   }.property(),
 
+  districtsByTerritory: function(key, value) {
+    if(this.selectedTerritory && this.selectedTerritory.id) {
+      return this.selectedTerritory.get('districts');
+    } else {
+      return this.store.findAll('district');
+    }
+  }.property('selectedTerritory'),
+
   actions: {
     saveContactDetails: function() {
       var addressProperties = this.getProperties('street', 'flat', 'building');
+      addressProperties.district = this.selectedDistrict;
       var address = this.store.createRecord('address', addressProperties);
 
       var contactProperties = this.getProperties('name', 'mobile');
