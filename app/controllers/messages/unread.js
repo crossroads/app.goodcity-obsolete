0a2d16f44a3ca3c12f2a59d2528b3d75fcc86bdc
Michael McDonald <none@none>
2014-09-24 17:36:38 +0800

    add behaviour for clicking global unread count for reviewers, hide global message count if none

--- app/controllers/messages/unread.js
+++ app/controllers/messages/unread.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-
+  
+  needs: ['authorize'],
   sortProperties: ['id'],
   sortAscending: false,
 
@@ -24,6 +25,11 @@ export default Ember.ArrayController.extend({
     },
 
     viewUnread: function() {
+      if (this.get('controllers.authorize.isReviewer')) {
+        this.transitionToRoute("inbox.under_review");
+        return;
+      }
+      
       var uniqueOfferIds = this.get('unread').mapBy('offerId').uniq();
 
       if (uniqueOfferIds.length > 1) {
