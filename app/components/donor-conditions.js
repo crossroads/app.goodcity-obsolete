0cda18c8f7067b176657f23126496f1a4f6b5e12
swatijadhav <swati.jadhav@kiprosh.com>
2014-09-25 18:52:42 +0530

    display donor-conditions in sorted in id-order

--- app/components/donor-conditions.js
+++ app/components/donor-conditions.js
@@ -2,12 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  donorConditions: function() {
-    var store = this.get('targetObject.store');
-    return store.find('donor_condition');
-    //~ .then( function(objs) {
-      //~ return objs.sortBy('id');
-    //~ })
-  }.property('donor_condition.[]'),
+  donorConditions: function(key, value) {
+
+    if (arguments.length > 1) {
+      return value;
+    } else {
+
+      var sortConditions, _this = this;
+      var store = this.get('targetObject.store');
+
+      store.find('donor_condition').then( function(objs) {
+        sortConditions = objs.sortBy('id');
+        _this.set("donorConditions", sortConditions);
+        value = sortConditions;
+      });
+      return value;
+    }
+  }.property('donor_condition.[]')
 
 });
