4f0e3bec1595dcffeda0441317f25ecc93e32aa1
swatijadhav <swati.jadhav@kiprosh.com>
2014-09-26 16:24:43 +0530

    display accepted items list on donation-details page

--- app/controllers/delivery/donation_details.js
+++ app/controllers/delivery/donation_details.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   needs: ['offer/index'],
 
+  confirmedItems: function(){
+    var items = this.get('offer.items');
+    return items.filterBy('state', 'accepted');
+  }.property('offer.items.@each'),
+
   actions:{
     cancelOffer: function(offer){
       this.get('controllers.offer/index').send('cancelOffer', offer);
