17ef13ac6f9365cafb968a4b15e1e6a1298a7318
swatijadhav <swati.jadhav@kiprosh.com>
2014-10-06 12:47:42 +0530

    revised code at mark unread messages as read

--- app/routes/offer/messages.js
+++ app/routes/offer/messages.js
@@ -15,11 +15,14 @@ export default AuthorizeRoute.extend({
 
   afterModel: function(messages) {
     var unreadMessages = messages.filterBy('state', 'unread');
-    unreadMessages.forEach(function(message) {
-      message.set('state', 'read');
-    });
+    var adapter = this.container.lookup('adapter:application');
 
-    return Ember.RSVP.all(unreadMessages.invoke('save'));
+    unreadMessages.forEach(function(message) {
+      var url = adapter.buildURL('message', message.get('id')) + '/mark_read';
+      adapter.ajax(url, 'PUT').then(function(response) {
+        message.setProperties(response.message);
+      });
+    });
   }
 
 });
