30fff74d6564dedcad5d0a38e6958ee3311c5545
swatijadhav <swati.jadhav@kiprosh.com>
2014-10-07 17:55:47 +0530

    Fixed issue at 'Reviewer start reviewing offer'
    - Hide 'Start Review' button for Supervisor
    - Fixed issue at assigning reviewer to offer
    - Fixed issue with same offer review status for all offers

--- app/controllers/review_offer/items.js
+++ app/controllers/review_offer/items.js
@@ -2,18 +2,21 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
 
-  needs: ['application'],
-
-  offer: function() {
-    var offerId =  this.get('content.firstObject.offerId');
-    return this.store.getById('offer', offerId);
-  }.property('items.[]'),
+  needs: ['application', 'offer'],
 
   reviewer: Ember.computed.alias('offer.reviewedBy'),
+  currentOfferId: Ember.computed.alias('controllers.offer.id'),
+  currentUser: Ember.computed.alias('controllers.application.currentUser'),
+  isReviewer: Ember.computed.alias('currentUser.isReviewer'),
+
+  offer: function() {
+    var offerId = this.get('currentOfferId');
+    return this.store.getById('offer', offerId);
+  }.property('currentOfferId'),
 
   actions: {
     startReview: function() {
-      var reviewedBy = this.get('controllers.application.currentUser');
+      var reviewedBy = this.get('currentUser');
       var offerId = this.get('offer.id');
 
       var offer = this.store.update('offer', {
