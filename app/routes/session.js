7da36d2768ffc805b0b61dcb23287a304b3139f3
swatijadhav <swati.jadhav@kiprosh.com>
2014-10-08 12:27:44 +0530

    fixed redirection of reviewer/supervisor to home-page if already logged-in

--- app/routes/session.js
+++ app/routes/session.js
@@ -3,7 +3,13 @@ import Ember from 'ember';
 var SessionRoute = Ember.Route.extend({
   beforeModel: function() {
     if (this.controllerFor('application').get('isLoggedIn')) {
-      this.transitionTo('/offers');
+      var permissions = JSON.parse(localStorage.permissions);
+
+      if(permissions['isReviewer'] || permissions['isSupervisor']) {
+        this.transitionTo('/inbox');
+      } else {
+        this.transitionTo('/offers');
+      }
     }
   }
 });
