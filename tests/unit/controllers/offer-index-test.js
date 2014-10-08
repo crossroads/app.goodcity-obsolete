bb6005de1b1d5de5466d1d05971406fe14a67ddd
Steve Kenworthy <steveyken@gmail.com>
2014-10-08 12:56:14 +0800

    Removed unneeded test for isDraft

--- tests/unit/controllers/offer-index-test.js
+++ tests/unit/controllers/offer-index-test.js
@@ -4,21 +4,3 @@ import { test, moduleFor } from 'ember-qunit';
 
 moduleFor('controller:offer/index', 'Offer Controller', {
 });
-
-test('isDraft value', function () {
-  expect(2);
-  var ctrl = this.subject();
-
-  Ember.run(function() {
-    ctrl.set('model', Ember.Object.create({ state: '' }));
-
-    // check the values before we modify the offer
-    equal(ctrl.get('isDraft'), false);
-
-    // modify the state of the offer
-    ctrl.get('model').set('state', 'draft');
-
-    // assert that the controllers isDraft has changed
-    equal(ctrl.get('isDraft'), true);
-  });
-});
