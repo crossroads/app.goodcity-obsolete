fbe6e36e07c7580ee688378658ba5ef1e6b11ee5
swatijadhav <swati.jadhav@kiprosh.com>
2014-10-09 19:41:25 +0530

    message alert notifications for few seconds

---
+++ app/views/messages/unread.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.View.extend({
+
+  notifyMessage: function(){
+    Ember.run.next(this, this.animateNotification);
+  }.observes('controller.unreadFirst').on('didInsertElement'),
+
+  animateNotification: function(){
+    Ember.$('.unread_message_box').show();
+    Ember.$('.unread_message_box').animate({"left": "2%"}, "slow");
+
+    window.setTimeout(function() {
+      Ember.$('.unread_message_box').fadeOut('slow');
+      Ember.$('.unread_message_box').animate({"left": "0"});
+    }, 6000);
+  }
+});
