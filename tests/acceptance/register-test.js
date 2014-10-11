d689825eb4911467028e4b157b6328d8c998b263
Michael McDonald <none@none>
2014-10-11 10:18:51 +0800

    fix register tests

--- tests/acceptance/register-test.js
+++ tests/acceptance/register-test.js
@@ -16,7 +16,7 @@ module('Acceptance: Register', {
   setup: function() {
     App = startApp();
     testHelper = TestHelper.setup(App);
-    logoutUser('/register');
+    delete window.localStorage.authToken;
     hk_user = FactoryGuy.build('with_hk_mobile');
   },
   teardown: function() {
@@ -29,68 +29,65 @@ module('Acceptance: Register', {
 
 test("All required registration details are filled", function() {
   expect(6);
+
   visit('/register');
-  fillIn('input#mobile', hk_user.mobile);
-  triggerEvent('input#mobile', 'blur');
-  fillIn('input#first_name',  hk_user.firstName );
-  fillIn('input#last_name', hk_user.lastName);
+  fillIn('#mobile', hk_user.mobile);
+  triggerEvent('#mobile', 'blur');
+  fillIn('#first_name',  hk_user.firstName );
+  fillIn('#last_name', hk_user.lastName);
+  click("#all");
+  click(find('select.ember-select>option:contains("Tung Chung")'));
 
   andThen(function() {
-    click(find("li.territory_menu#all")[0]);
-  });
-  andThen(function() {
-    click($('select.ember-select>option:contains("Tung Chung")').prop('selected', 'selected'));
-  });
-  andThen(function() {
-    equal($('select.ember-select').find(":selected").text(), "Tung Chung");
-  });
-  andThen(function() {
-    equal(find('input#mobile')[0].value, hk_user.mobile);
-    triggerEvent('input#mobile', 'blur');
-    equal(find('input#first_name')[0].value, hk_user.firstName);
-    equal(find('input#last_name')[0].value, hk_user.lastName);
-    equal($('select.ember-select').find(":selected").text(), "Tung Chung");
-    click($("button:contains('Register')")[0]);
-    andThen(function(){
-      equal(currentURL(), "/authenticate");
-    });
+    equal(find('select.ember-select').find(":selected").text(), "Tung Chung");
+    equal(find('#mobile').val(), hk_user.mobile);
   });
 
+  triggerEvent('#mobile', 'blur');
+
+  andThen(function() {
+    equal(find('#first_name').val(), hk_user.firstName);
+    equal(find('#last_name').val(), hk_user.lastName);
+    equal(find('select.ember-select').find(":selected").text(), "Tung Chung");
+  });
+
+  click("button:contains('Register')");
+
+  andThen(function(){
+    equal(currentURL(), "/authenticate");
+  });
 });
 
 test("cannot register unless mobile number details are entered", function() {
   expect(1);
   visit('/register');
-  fillIn("input#first_name",  hk_user.firstName );
-  fillIn('input#last_name', hk_user.lastName);
+  fillIn("#first_name",  hk_user.firstName );
+  fillIn('#last_name', hk_user.lastName);
   andThen(function() {
-    equal($('span:contains("Register")').closest('button').attr("disabled"), "disabled");
+    equal(currentURL(), '/register');
   });
 });
 
 test("should prepend country code +852 internally for api call", function() {
   expect(3);
   visit('/register');
+  fillIn('#mobile', hk_user.mobile);
+  triggerEvent('#mobile', 'blur');
   andThen(function() {
-    fillIn('input#mobile', hk_user.mobile);
-    triggerEvent('input#mobile', 'blur');
-
-  });
-  andThen(function() {
-    equal(find('input#mobile')[0].value, hk_user.mobile);
-    equal(find('input#mobile')[0].getAttribute("data-actual-mobile").substr(0,4), '+852');
-    equal($('#mobile_error').text(), "");
+    equal(find('#mobile').val(), hk_user.mobile);
+    equal(find('#mobile').attr("data-actual-mobile").substr(0,4), '+852');
+    equal(find('#mobile_error').text(), "");
   });
 });
 
 test("mobile number length should be 8 digit (excluding country code)", function() {
   expect(1);
   visit('/register');
-  fillIn('input#mobile', hk_user.mobile);
-  triggerEvent('input#mobile', 'blur');
-  fillIn("input#first_name",  hk_user.firstName );
-  fillIn('input#last_name', hk_user.lastName);
+  fillIn('#mobile', hk_user.mobile);
+  triggerEvent('#mobile', 'blur');
+  fillIn("#first_name",  hk_user.firstName );
+  fillIn('#last_name', hk_user.lastName);
   andThen(function() {
-    equal(find('input#mobile')[0].value.length, 8);
+    equal(find('#mobile').val().length, 8);
   });
 });
