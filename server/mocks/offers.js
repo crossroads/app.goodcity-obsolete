9de0bd6a1d19a6597e664beb83b9f538f75d9a1b
Michael McDonald <none@none>
2014-10-11 15:51:27 +0800

    format offers json sample data

--- server/mocks/offers.js
+++ server/mocks/offers.js
@@ -6,13 +6,27 @@ module.exports = function(app) {
   var image_id = "1407764294/default/test_image.jpg";
 
   var offers_json = {
-    "offers": [ {"id": "1", "state": "draft", "collection_contact_name": 'TestOffer', "item_ids": ["1", "2"] },
-      {"id": "2", "state": "draft"} ],
-    "items": [{"id": "1", "donor_description":'example1', "offer_id": "1", "image_ids": ["1", "2"]}, {"id": "2", "donor_description":'example2', "offer_id": "1"}],
-    "images": [{"id": "1", "image_url": image_url, "thumb_image_url": image_url, "favourite": "false", "image_id": image_id },
-      {"id": "2", "image_url": image_url, "thumb_image_url": image_url, "favourite": "true", "image_id": image_id }],
-    "donor_conditions": [{ "id": 1, "name": "New" },{"id": 4,"name": "Broken"},{"id": 2,"name": "Lightly Used"},{"id": 3,"name": "Heavily Used"}]
-
+    "offers": [
+      { "id": "1", "state": "draft", "collection_contact_name": 'TestOffer', "item_ids": ["1", "2"], "delivery_id": null },
+      { "id": "2", "state": "draft", "delivery_id": null }
+    ],
+    "items": [
+      { "id": "1", "donor_description": 'example1', "offer_id": "1", "image_ids": ["1", "2"] },
+      { "id": "2", "donor_description": 'example2', "offer_id": "1" }
+    ],
+    "images": [
+      { "id": "1", "image_url": image_url, "thumb_image_url": image_url, "favourite": "false", "image_id": image_id },
+      { "id": "2", "image_url": image_url, "thumb_image_url": image_url, "favourite": "true", "image_id": image_id }
+    ],
+    "donor_conditions": [
+      { "id": 1, "name": "New" },
+      { "id": 4, "name": "Broken" },
+      { "id": 2, "name": "Lightly Used" },
+      { "id": 3, "name": "Heavily Used" }
+    ],
+    "addresses": [
+      { "id": 1, "street": null, "flat": null, "building": null, "district_id": null, "addressable_id": 14, "addressable_type": "User" }
+    ]
   };
 
   offersRouter.get('/', function(req, res) {
