93cc7530c4b4504d103b40fac5cb60d696034928
Michael McDonald <mcm-ham@users.noreply.github.com>
2014-10-25 10:36:04 +0800

    add offer notification handling

--- app/controllers/notifications.js
+++ app/controllers/notifications.js
@@ -6,6 +6,7 @@ export default Ember.ArrayController.extend({
   model: [],
 
   mostRecent: function() {
+    //retrieveMostRecent is not implemented here because it needs to call itself
     return this.retrieveMostRecent();
   }.property('model.[]'),
 
@@ -32,22 +33,24 @@ export default Ember.ArrayController.extend({
   setRoute: function(notification) {
     if (notification.entity_type === "message") {
       this.setMessageRoute(notification);
+    } else if (notification.entity_type === "offer") {
+      notification.route = ["offer", notification.entity.id];
     }
   },
 
   setMessageRoute: function(notification) {
-    //note notification.entity is raw from api so no 'get' method and properties 'offer_id' not 'offerId'
+    //note notification.entity has not been normalized thru ember so no 'get' method
+    //and properties are 'offer_id' not 'offerId'
+
     var message = notification.entity;
     var offerId = message.offer_id;
 
     if(this.get('session.currentUser.isDonor')) {
       notification.route = ["offer.messages", offerId];
+    } else if (message.is_private) {
+      notification.route = ["review_offer.supervisor_messages", offerId];
     } else {
-      if (message.is_private) {
-        notification.route = ["review_offer.supervisor_messages", offerId];
-      } else {
-        notification.route = ["review_offer.messages", offerId];
-      }
+      notification.route = ["review_offer.messages", offerId];
     }
   },
 
