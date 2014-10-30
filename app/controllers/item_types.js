4cca89306b3d5b20690352fedf3d0a4f1684c1ba
Shivani Thakur <shivanibhanwal@gmail.com>
2014-10-31 00:00:23 +0530

    removed function as we do not need it at the moment

--- app/controllers/item_types.js
+++ app/controllers/item_types.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  listOfItemTypes: function() {
-    return this.get('itemTypes');
-  }.property('itemTypes.[]'),
+  // listOfItemTypes: function() {
+  //   return this.get('itemTypes');
+  // }.property('itemTypes.[]'),
 });
