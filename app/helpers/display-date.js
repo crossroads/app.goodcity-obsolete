16596d1cce2de98b8bec3238824dda860c7c6c2e
swatijadhav <swati.jadhav@kiprosh.com>
2014-11-03 16:33:25 +0530

    add CSS for select time page and fixed highlighting of radio-buttons

--- app/helpers/display-date.js
+++ app/helpers/display-date.js
@@ -2,5 +2,5 @@ import Ember from "ember";
 
 export default Ember.Handlebars.makeBoundHelper(function(value) {
   var parseDate = Date.parse(value);
-  return moment(parseDate).format('LL');
+  return moment(parseDate).format('dddd, MMMM Do');
 });
