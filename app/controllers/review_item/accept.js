9eb5118b2a5db63a4667f2d1f4a452c0281e3b59
Shivani Thakur <shivanibhanwal@gmail.com>
2014-11-12 10:13:14 +0530

    reset the item type when user select another item or offer

--- app/controllers/review_item/accept.js
+++ app/controllers/review_item/accept.js
@@ -2,12 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  needs: ['review_item'],
+  needs: ["review_item", "offer"],
 
   itemTypeId: Ember.computed.alias('controllers.review_item.reviewItemTypeId'),
   itemTypeName:  Ember.computed.alias('controllers.review_item.reviewItemTypeName'),
+  defaultImageId: Ember.computed.alias('controllers.review_item.defaultImageId'),
+
+  reviewItemDidChange: function() {
+    this.set('itemTypeId', '');
+    this.set('itemTypeName', '');
+  }.observes('controllers.review_item.each'),
 
   actions: {
+    acceptOffer: function(){
+      alert("accept");
+    }
 
   }
 });
