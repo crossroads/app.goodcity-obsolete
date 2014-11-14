c75edc0051f49bb22f52283ea535ca63508de902
swatijadhav <swati.jadhav@kiprosh.com>
2014-11-14 16:40:01 +0530

    refactored review-offer/supervisor-messages controller

--- app/controllers/review_offer/supervisor_messages.js
+++ app/controllers/review_offer/supervisor_messages.js
@@ -1,31 +1,14 @@
 import Ember from 'ember';
+import sendMessage from './../send_message';
 
-export default Ember.ArrayController.extend({
-
-  needs: ["offer"],
-  sortProperties: ['createdAt'],
-  sortAscending: true,
-  offerMessage: true,
+export default sendMessage.extend({
 
   filteredContent: Ember.computed.filterBy('arrangedContent', 'isPrivate'),
 
   actions: {
     sendMessage: function() {
-      var offer_id = this.get('controllers.offer').get('id');
-      var offer = this.store.getById('offer', offer_id);
-
-      var newMessageProperties = this.getProperties('body');
-      newMessageProperties.offer = offer;
-      newMessageProperties.isPrivate = true;
-      newMessageProperties.createdAt = Date.now();
-
-      this.set('body', '');
-
-      var message = this.store.createRecord('message', newMessageProperties);
-      message.save().then(function(message){
-        message.reload();
-      });
+      this._super(true);
     }
   }
-
 });
+
