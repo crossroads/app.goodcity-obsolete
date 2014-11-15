cf6d17ce82c7540af3413497dfcad2b6a549300a
Michael McDonald <mcm-ham@users.noreply.github.com>
2014-11-16 07:22:02 +0800

    Restore missing name initial on inbox removed by mistake Remove UserPermission mixin shared between user and user_profile since properties were only being used by one or the other Remove user_profile.image property since it's not used

--- app/models/user.js
+++ app/models/user.js
@@ -1,9 +1,19 @@
 import DS from 'ember-data';
-import UserPermissionMixin from '../mixins/user-permission';
 
 var attr = DS.attr;
 
-export default DS.Model.extend(UserPermissionMixin, {
+export default DS.Model.extend({
   firstName:   attr('string'),
-  lastName:    attr('string')
+  lastName:    attr('string'),
+
+  permission:  DS.belongsTo('permission'),
+
+  nameInitial: function() {
+    return this.get('firstName').charAt(0).capitalize();
+  }.property('firstName'),
+
+  roleInitials: function(){
+    return this.get('isDonor') ? "(D)" :
+      "("+ this.get("permission.name").capitalize().charAt(0) +")";
+  }.property('permission')
 });
