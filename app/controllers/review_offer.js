e812ea05cc3659cbd0ddbe22601b85e92660ed4c
swatijadhav <swati.jadhav@kiprosh.com>
2014-11-15 18:53:16 +0530

    fixed back link on review-offer page

---
+++ app/controllers/review_offer.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  offer: Ember.computed.alias('model'),
+  isSubmittedOffer: Ember.computed.alias('offer.isSubmitted'),
+});
