fda11851ac19dfb32090d62da315bab1b93d49a3
swatijadhav <swati.jadhav@kiprosh.com>
2014-11-20 16:05:32 +0530

    fixed calender view css

--- app/views/select-date.js
+++ app/views/select-date.js
@@ -3,14 +3,13 @@ import Ember from 'ember';
 export default Ember.View.extend({
   tagName: 'input',
   classNames: 'pickadate',
-  type: 'text',
 
   didInsertElement: function(){
     Ember.$().ready(function(){
       Ember.$('.pickadate').pickadate({
         format: 'ddd mmm d',
-        // Disable Sundays, Mondays, 27-november-2014
-        disable: [ 1, 2, new Date(2014,10,28) ],
+        // Disable Sundays, Mondays, 28-november-2014
+        disable: [ 1, 2, [2014,10,28] ],
         clear: false,
         today: false,
         close: false,
