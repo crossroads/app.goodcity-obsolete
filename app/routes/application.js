482e7b084d0ff336b8d9b210086bb397017d80b2
Michael McDonald <mcm-ham@users.noreply.github.com>
2014-11-27 16:23:18 +0800

    Move sync-data to ApplicationRoute so no longer need to duplicate error handling and is easier to mock in tests

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,10 +1,35 @@
 import Ember from 'ember';
+import AjaxPromise from '../utils/ajax-promise';
+import config from '../config/environment';
 
 export default Ember.Route.extend({
 
   beforeModel: function () {
+    var _this = this;
+
     var language = localStorage.language || Ember.I18n.default_language;
     Ember.I18n.translations = Ember.I18n.translation_store[language];
+
+    Ember.RSVP.on('error', function(error) {
+      _this.send('error', error);
+    });
+
+    //preload data
+    var retrieve = function(types) {
+      return types.map(function(type) { return _this.store.find(type); });
+    };
+    var promises = retrieve(config.APP.PRELOAD_TYPES);
+
+    //if logged in
+    if (_this.session.get('authToken')) {
+      promises.push(
+        new AjaxPromise("/auth/current_user_profile", "GET", _this.session.get("authToken"))
+          .then(function(data) { _this.store.pushPayload(data); })
+      );
+      promises = promises.concat(retrieve(config.APP.PRELOAD_AUTHORIZED_TYPES));
+    }
+
+    return Ember.RSVP.allSettled(promises);
   },
 
   renderTemplate: function() {
@@ -29,7 +54,23 @@ export default Ember.Route.extend({
       Ember.I18n.translations = Ember.I18n.translation_store[language];
       localStorage.language = language;
       window.location.reload();
+    },
+    loading: function() {
+      var view = this.container.lookup('view:loading').append();
+      this.router.one('didTransition', view, 'destroy');
+    },
+    error: function(reason) {
+      if (reason.status === 401) {
+        if (this.get('isLoggedIn')) {
+          this.send('logMeOut');
+        }
+        else {
+          this.transitionToRoute('login');
+        }
+      } else {
+        alert('Something went wrong');
+        Ember.Logger.error(reason);
+      }
     }
   }
-
 });
