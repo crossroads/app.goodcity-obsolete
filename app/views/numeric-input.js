3e802448d6b36ebb6dec9f4bb4c8a4393589cc8a
swatijadhav <swati.jadhav@kiprosh.com>
2014-12-03 17:38:19 +0530

    fixed issue with numeric input

--- app/views/numeric-input.js
+++ app/views/numeric-input.js
@@ -2,12 +2,35 @@ import Ember from 'ember';
 
 export default Ember.TextField.extend({
   tagName: "input",
-  type: "number",
+  type: "tel",
   attributeBindings: [ "name", "type", "value", "maxlength", "id" ],
 
-  keyDown: function(e) {
+  currentKey: function(key, value){
+    return (arguments.length > 1) ? value : 0;
+  }.property(),
 
+  isAllowed: function(){
+    var key = this.get('currentKey');
+    var allowed = (key === 13 ||
+      key === 8 ||
+      key === 9 ||
+      key === 46 ||
+      key === 39 ||
+      (key >= 35 && key <= 37));
+    return allowed;
+  }.property('currentKey'),
+
+  keyUp: function(){
+    var value = this.value;
+    if(value.search(/^\d{8}$/) !== 0){
+      this.set('value', value.replace(/\D/g,''));
+    }
+  },
+
+  keyDown: function(e) {
     var key = e.charCode || e.keyCode || 0;
+    this.set('currentKey', key);
+
     // allow ctrl+v, enter, backspace, tab, delete, numbers, keypad numbers
     // home, end only.
     return (
@@ -22,23 +45,8 @@ export default Ember.TextField.extend({
         (key >= 96 && key <= 105));
   },
 
-  keyPress: function(e) {
-    var key = e.charCode || e.keyCode || 0;
+  keyPress: function() {
     var inputValue = this.value || "";
-    var allowed = this.send('isAllowed', key);
-    console.log(allowed);
-    return allowed ? true : (inputValue.length < this.maxlength);
-  },
-
-  actions: {
-    isAllowed: function(key) {
-      var allowed = (key === 13 ||
-        key === 8 ||
-        key === 9 ||
-        key === 46 ||
-        key === 39 ||
-        (key >= 35 && key <= 37));
-      return allowed;
-    }
+    return this.get('isAllowed') ? true : (inputValue.length < this.maxlength);
   }
 });
