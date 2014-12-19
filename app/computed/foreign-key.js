ce4fa97e44275b9066f6f37a1cf45224e008d5d8
Michael McDonald <mcm-ham@users.noreply.github.com>
2014-12-19 17:16:08 +0800

    Add documentation to foreignKey computed property

--- app/computed/foreign-key.js
+++ app/computed/foreign-key.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
+// Example usage:
+//
+// DS.Model.extend({
+//   donorCondition:       DS.belongsTo('donor_condition'),
+//   donorConditionId:     Ember.computed.foreignKey('donorCondition.id'),
+// });
+//
+// This allows you to set `donorConditionId` which will update the `donorCondition`
+// property. Binding `donorCondition.id` is not good because if the value changes
+// it'll update the `id` property on donorCondition instead of the donorCondition
+// on the model.
+
 export default Ember.computed.foreignKey = function(path) {
   var relationshipName = path.split('.')[0];
   return Ember.computed(function(key, value) {
