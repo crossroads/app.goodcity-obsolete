05259861f58aaaa49d9484dfc2bb2a278f0b7e2c
swatijadhav <swati.jadhav@kiprosh.com>
2014-12-22 19:36:20 +0530

    fixed offer-index, reject-offer and review-status view for reviewed offers

--- app/controllers/offer/review_status.js
+++ app/controllers/offer/review_status.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
 
-  awaitingReview: Ember.computed.filterBy('content', 'state', 'submitted'),
-  rejected: Ember.computed.filterBy('content', 'state', 'rejected'),
+  awaitingReview: Ember.computed.filterBy('items', 'state', 'submitted'),
+  rejected: Ember.computed.filterBy('items', 'state', 'rejected'),
+  accepted: Ember.computed.filterBy('items', 'state', 'accepted'),
 
 });
--- app/routes/offer/review_status.js
+++ app/routes/offer/review_status.js
@@ -1,11 +1,4 @@
 import AuthorizeRoute from './../authorize';
 
 export default AuthorizeRoute.extend({
-  model: function() {
-    var offerId = this.modelFor('offer').get('id');
-
-    return this.store.filter('item', function(item) {
-      return item.get('offer.id') === offerId;
-    });
-  }
 });
