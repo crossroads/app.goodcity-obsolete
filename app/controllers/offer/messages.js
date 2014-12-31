8c69bfec847b2ba2e70a86ed975d2fc16bba6d66
swatijadhav <swati.jadhav@kiprosh.com>
2014-12-31 17:39:09 +0530

    fixed back link at offer-messages

--- app/controllers/offer/messages.js
+++ app/controllers/offer/messages.js
@@ -2,6 +2,12 @@ import Ember from 'ember';
 import sendMessage from './../send_message';
 
 export default sendMessage.extend({
+  needs: ['offer'],
+
+  offer: function(){
+    var offerId = this.get('controllers.offer').get('id');
+    return this.store.getById('offer', offerId);
+  }.property('controllers.offer.id'),
 
   displayMessage: true,
   filteredContent: Ember.computed.filterBy('allMessages', 'isPrivate', false),
