7a282765a78d06184b27e366863f44b94e29c010
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-09 18:28:52 +0530

    fixed issue with creation of new offer
    - while creating new offer, only select offers with "draft" state and has no items

--- app/controllers/offers/index.js
+++ app/controllers/offers/index.js
@@ -7,7 +7,7 @@ export default Ember.ArrayController.extend({
 
   actions: {
     newOffer: function(){
-      var empty_offers = this.filterBy('itemCount', 0).sortBy('id');
+      var empty_offers = this.filterBy('itemCount', 0).filterBy('state', 'draft').sortBy('id');
       if (empty_offers.length === 0){
         this.transitionToRoute('offers.new');
       } else {
