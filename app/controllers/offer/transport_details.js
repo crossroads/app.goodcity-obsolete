0692195891febef06fbf851f31a5c5c515eb105e
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-13 17:27:56 +0530

    transport details page changes for scheduled offers

--- app/controllers/offer/transport_details.js
+++ app/controllers/offer/transport_details.js
@@ -1,16 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  delivery: Ember.computed.alias('model.delivery'),
+
   user: function(){
     var userId = this.session.get("currentUser.id");
     return this.store.getById('user_profile', userId);
   }.property().volatile(),
 
   userName: function(){
-    return this.get('contact.name') || this.get("user.fullName");
+    return this.get('delivery.contact.name') || this.get("user.fullName");
   }.property('contact.name', 'user'),
 
   userMobile: function(){
-    return this.get('contact.mobile') || this.get("user.mobile");
+    return this.get('delivery.contact.mobile') || this.get("user.mobile");
   }.property('contact.mobile', 'user'),
+
+  district: function(){
+    return this.get('delivery.contact.address.district.name') || this.get("user.address.district.name");
+  }.property('user', 'delivery')
 });
