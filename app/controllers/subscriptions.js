9cefb12fc2caa424fefa388097b4829daeea5e55
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-16 20:09:10 +0530

    fixed dupliaction of records in data-store due to side loading

--- app/controllers/subscriptions.js
+++ app/controllers/subscriptions.js
@@ -30,17 +30,18 @@ export default Ember.Controller.extend(EmberPusher.Bindings, {
       var type = Object.keys(data.item)[0];
       var item = this.store.normalize(type, data.item[type]);
       var existingItem = this.store.getById(type, item.id);
-      item['id'] = item['id'] || item[Object.keys(item)[0]].id;
-
-      // if (sender.id === parseInt(this.session.get("currentUser.id"))) {
-      //   return;
-      // }
 
       if (!this.store.hasRecordForId("user", sender.id)) {
         this.store.push("user", sender);
       }
 
       if (data.operation === "create" && !existingItem) {
+        var dirtyRecords = this.store.all(type).filterBy('id', null);
+        if(dirtyRecord.length > 0) {
+          dirtyRecords.forEach(function(record){
+            record.unloadRecord();
+          });
+        }
         this.store.push(type, item);
       } else if (data.operation === "update" && !existingItem) {
         this.store.find(type, item.id);
