ef2a7a2eb9b0569a076675fdd52be2dae9b470f8
swatijadhav <swati.jadhav@kiprosh.com>
2015-01-22 19:49:57 +0530

    add css changes for draft-offer-details view

--- app/routes/offer/index.js
+++ app/routes/offer/index.js
@@ -2,4 +2,10 @@ import AuthorizeRoute from './../authorize';
 
 export default AuthorizeRoute.extend({
   controllerName: 'offer/offer_details',
+
+  afterModel: function(my_offer) {
+    if(my_offer.get('itemCount') > 0) {
+      this.transitionTo('offer.offer_details', my_offer);
+    }
+  },
 });
