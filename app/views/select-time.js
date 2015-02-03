86e82976ce1c764deee98dc29c20270764358980
swatijadhav <swati.jadhav@kiprosh.com>
2015-02-03 13:48:48 +0530

    restrict elapsed time-slots for current-day value

--- app/views/select-time.js
+++ app/views/select-time.js
@@ -10,9 +10,8 @@ export default Ember.TextField.extend({
     Ember.$().ready(function(){
       Ember.$('.timepicker').pickatime({
         clear: '',
-        min: [8,30],
+        min: [10,0],
         max: [16,0],
-        // editable: true,
         onSet: function() {
           var slot = this.get("select") && this.get("select").time;
           _this.set("selection", slot);
@@ -22,7 +21,33 @@ export default Ember.TextField.extend({
           if(slot) {
             this.set('select', slot);
           }
-        }
+        },
+        onOpen: function() {
+          var selectedDate = Ember.$('.pickadate').val();
+          var currentDate = new Date();
+          var currentYear = currentDate.getFullYear().toString();
+          selectedDate = new Date(selectedDate + " " + currentYear);
+          currentDate.setHours(0,0,0,0);
+
+          if(selectedDate.getTime() === currentDate.getTime()) {
+            var currentTime = new Date();
+            var hours = currentTime.getHours();
+            var minutes = currentTime.getMinutes();
+
+            if(minutes > 30) {
+              hours += 1; minutes = 0;
+            } else {
+              minutes = 30;
+            }
+
+            var totalMins = hours*60 + minutes;
+
+            this.set('min', {hour: hours, mins: minutes, time: totalMins, pick: totalMins});
+            this.set('select', null);
+          } else {
+            this.set('min', {hour: 10, mins: 0, time: 600, pick: 600});
+          }
+        },
       });
     });
   }
