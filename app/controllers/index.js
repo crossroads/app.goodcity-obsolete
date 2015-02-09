c675f720b6da031978b15ea3a3efdc262c5df433
swatijadhav <swati.jadhav@kiprosh.com>
2015-02-09 20:39:14 +0530

    WIP changes in tour pages

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   needs: ['application'],
-  currentLanguage: Ember.computed.alias('controllers.application.currentLanguage')
+  currentLanguage: Ember.computed.alias('controllers.application.currentLanguage'),
+
+  scrollToBottom: function() {
+    window.scrollTo(0,document.body.scrollHeight);
+  }.on("init"),
 });
