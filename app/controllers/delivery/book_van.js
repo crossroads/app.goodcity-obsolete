521e83ae865836a3cf5ed8da2557056c6484ecf0
swatijadhav <swati.jadhav@kiprosh.com>
2015-02-12 15:18:03 +0530

    GCW-140 comment# remove pickadate time selector and add custom select list for time-slot-list

--- app/controllers/delivery/book_van.js
+++ app/controllers/delivery/book_van.js
@@ -5,6 +5,21 @@ import addressDetails from './address_details';
 export default addressDetails.extend({
   needs: ['delivery'],
 
+  timeSlots: [
+    {id: "600", name: "10:00 AM" },
+    {id: "630", name: "10:30 AM"},
+    {id: "690", name: "11:00 AM"},
+    {id: "720", name: "11:30 AM"},
+    {id: "750", name: "12:00 PM"},
+    {id: "780", name: "1:00 PM"},
+    {id: "810", name: "1:30 PM"},
+    {id: "840", name: "2:00 PM"},
+    {id: "870", name: "2:30 PM"},
+    {id: "900", name: "3:00 PM"},
+    {id: "930", name: "3:30 PM"},
+    {id: "960", name: "4:00 PM"},
+  ],
+
   selectedDate: null,
   selectedTime: null,
   speakEnglish: false,
@@ -19,11 +34,11 @@ export default addressDetails.extend({
       var deliveryId = controller.get('controllers.delivery').get('id');
       var delivery = controller.store.getById('delivery', deliveryId);
 
-      selectedDate.setMinutes(selectedDate.getMinutes() + controller.get('selectedTime'));
+      selectedDate.setMinutes(selectedDate.getMinutes() + parseInt(controller.get('selectedTime.id')));
 
       var requestProperties = {};
       requestProperties.pickupTime = selectedDate;
-      requestProperties.slot = Ember.$('#selectedTime').val();
+      requestProperties.slot = this.get('selectedTime.name');
       requestProperties.districtId = controller.get('selectedDistrict.id');
       requestProperties.territoryId = controller.get('selectedTerritory.id');
       requestProperties.needEnglish = controller.get("speakEnglish");
