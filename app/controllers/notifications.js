d1fd19bfae1c25fdc99daeed5fa8895ea0e19a97
Michael McDonald <mcm-ham@users.noreply.github.com>
2015-02-18 10:29:03 +0800

    Fix issue where timer resets if new notification arrives Move message mark read to update_store event instead of notification so code runs as soon as message arrives not when the notification is displayed which can be many seconds later

--- app/controllers/notifications.js
+++ app/controllers/notifications.js
@@ -1,34 +1,18 @@
-import Ember from 'ember';
-import readMessageMixin from '../mixins/read-message';
+import Ember from "ember";
+import messagesUtil from "../utils/messages";
 
-export default Ember.ArrayController.extend(readMessageMixin, {
-  sortProperties: ['date'],
+export default Ember.ArrayController.extend({
+  sortProperties: ["date"],
   sortAscending: true,
-  showItemImage: false,
-  itemImageUrl: null,
-  senderImageUrl: null,
 
   nextNotification: function() {
     //retrieveNextNotification is not implemented here because it needs to call itself
     return this.retrieveNextNotification();
-  }.property('[]'),
-
-  showItem: function(notification) {
-    var itemId = notification.entity.item_id;
-    var url;
-    this.set("showItemImage", itemId);
-    if(itemId){
-      url = this.store.getById('item', itemId).get("displayImageUrl");
-      this.set('itemImageUrl', url);
-    }
-
-    var userId = notification.entity_type === "message" ? notification.entity.sender_id : notification.entity.created_by_id;
-    url = this.store.getById("user", userId).get("displayImageUrl");
-    this.set('senderImageUrl', url);
-  },
+  }.property("[]"),
 
   retrieveNextNotification: function() {
-    var notification = this.get('firstObject');
+    // not sure why but model.firstObject is undefined when there's one notification
+    var notification = this.get("model")[0];
     if (!notification) {
       return null;
     }
@@ -38,54 +22,38 @@ export default Ember.ArrayController.extend(readMessageMixin, {
     // if current url matches notification view action url then dismiss notification
     var router = this.get("target");
     var currentUrl = router.get("url");
-    var notificationUrl = router.generate.apply(router, notification.route);
-    if (currentUrl === notificationUrl) {
-      if (notification.entity_type === "message"){
-        var message = this.store.getById("message", notification.entity.id);
-        this.markMessageAsRead(message);
-      }
+    var actionUrl = router.generate.apply(router, notification.route);
+
+    if (currentUrl === actionUrl) {
       this.removeObject(notification);
       return this.retrieveNextNotification();
     }
-    this.showItem(notification);
+
     return notification;
   },
 
+  itemImageUrl: function() {
+    var itemId = this.get("nextNotification.entity.item_id");
+    return itemId ? this.store.getById("item", itemId).get("displayImageUrl") : null;
+  }.property("nextNotification"),
+
+  showItemImage: Ember.computed.notEmpty("itemImageUrl"),
+
+  senderImageUrl: function() {
+    var notification = this.get("nextNotification");
+    if (!notification) { return null; }
+    var userId = notification.entity_type === "message" ?
+      notification.entity.sender_id :
+      notification.entity.created_by_id;
+    return this.store.getById("user", userId).get("displayImageUrl");
+  }.property("nextNotification"),
+
   setRoute: function(notification) {
     if (notification.entity_type === "message") {
-      this.setMessageRoute(notification);
+      notification.route = messagesUtil.getRoute(this.container, notification.entity);
     } else if (notification.entity_type === "offer") {
       var routeName = this.get("session.currentUser.isDonor") ? "offer" : "review_offer";
       notification.route = [routeName, notification.entity.id];
-    }
-  },
-
-  setMessageRoute: function(notification) {
-    //note notification.entity has not been normalized thru ember so no 'get' method
-    //and properties are 'offer_id' not 'offerId'
-
-    var message = notification.entity;
-    var offerId = message.offer_id;
-    var itemId = message.item_id;
-
-    if(this.get('session.currentUser.isDonor')) {
-      if(itemId) {
-        notification.route = ["item.messages", offerId, itemId];
-      } else{
-        notification.route = ["offer.messages", offerId];
-      }
-    } else if (message.is_private) {
-      if(itemId) {
-        notification.route = ["review_item.supervisor_messages", offerId, itemId];
-      } else{
-        notification.route = ["offer.supervisor_messages", offerId];
-      }
-    } else {
-      if(itemId) {
-        notification.route = ["review_item.donor_messages", offerId, itemId];
-      } else{
-        notification.route = ["offer.donor_messages", offerId];
-      }
     }
   },
 
--- app/views/notifications.js
+++ app/views/notifications.js
@@ -1,26 +1,33 @@
 import Ember from "ember";
 
 export default Ember.View.extend({
-  timer: 0,
-
   notifyMessage: function(){
     Ember.run.once(this, this.animateNotification);
   }.observes("controller.[]").on("didInsertElement"),
 
   animateNotification: function(){
-    Ember.run.cancel(this.get("timer"));
-    var box = Ember.$('.contain-to-grid');
+    var box = Ember.$(".contain-to-grid");
     var notification = this.get("controller.nextNotification");
     if (!notification) { box.hide(); return; }
-    if (box.is(":hidden")) { box.slideDown(); }
+    if (box.is(":hidden")) {
+      box.slideDown();
+      Ember.run.later(this, this.removeNotification, box, notification, 6000);
+    }
+  },
+
+  removeNotification: function(box, notification) {
     var controller = this.get("controller");
-    var removeNotification = function() {
-      if (controller.get("length") === 1) {
-        box.slideUp(400, Ember.run.bind(this, function() { controller.removeObject(notification); }));
+    var remove = function() { controller.removeObject(notification); };
+    if (controller.get("length") === 1) {
+      box.slideUp(400, remove);
+    } else {
+      remove();
+      notification = controller.get("nextNotification");
+      if (notification) {
+        Ember.run.later(this, this.removeNotification, box, notification, 6000);
       } else {
-        controller.removeObject(notification);
+        box.slideUp(400);
       }
-    };
-    this.set("timer", Ember.run.later(this, removeNotification, 6000));
+    }
   }
 });
