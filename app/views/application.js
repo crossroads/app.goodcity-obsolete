f22e1f3734272e8a7b7a72f487f31720bc59e960
swatijadhav <swati.jadhav@kiprosh.com>
2015-02-19 16:24:47 +0530

    GCW-133 comment# using navigator.onLine verify user is online/offline and display message if he is offline

--- app/views/application.js
+++ app/views/application.js
@@ -1,5 +1,21 @@
 import Init from './init';
+import Ember from 'ember';
 
 export default Init.extend({
+  didInsertElement: function() {
+    var view = this;
+    window.addEventListener('online',  view.updateOnlineStatus);
+    window.addEventListener('offline', view.updateOnlineStatus);
+  },
+
+  updateOnlineStatus: function() {
+    var status = Ember.$("#status");
+    if(!navigator.onLine) {
+      status.removeClass("online").addClass("offline");
+      status.text("Attempting to connect to app.goodcity.hk...");
+    } else {
+      status.removeClass("offline").addClass("online");
+    }
+  },
 
 });
